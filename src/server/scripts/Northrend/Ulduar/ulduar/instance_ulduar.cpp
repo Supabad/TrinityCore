ce29bfa7766ff4c85e48538349636e9315e44451
Supabad <Supabad.trinity@gmail.com>
2011-07-03 20:38:26 +0200

    Scripts/Ulduar: Cleanup in Flame Leviathan script

--- src/server/scripts/Northrend/Ulduar/ulduar/instance_ulduar.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/instance_ulduar.cpp
@@ -390,23 +390,20 @@ class instance_ulduar : public InstanceMapScript
                     switch (eventId)
                     {
                         case EVENT_TOWER_OF_STORM_DESTROYED:
-                            FlameLeviathan->AI()->DoAction(1);
+                            FlameLeviathan->AI()->DoAction(ACTION_TOWER_OF_STORM_DESTROYED);
                             break;
                         case EVENT_TOWER_OF_FROST_DESTROYED:
-                            FlameLeviathan->AI()->DoAction(2);
+                            FlameLeviathan->AI()->DoAction(ACTION_TOWER_OF_FROST_DESTROYED);
                             break;
                         case EVENT_TOWER_OF_FLAMES_DESTROYED:
-                            FlameLeviathan->AI()->DoAction(3);
+                            FlameLeviathan->AI()->DoAction(ACTION_TOWER_OF_FLAMES_DESTROYED);
                             break;
                         case EVENT_TOWER_OF_LIFE_DESTROYED:
-                            FlameLeviathan->AI()->DoAction(4);
+                            FlameLeviathan->AI()->DoAction(ACTION_TOWER_OF_LIFE_DESTROYED);
                             break;
                     }
             }
 
-            void ProcessEvent(Unit* /*unit*/, uint32 /*eventId*/)
-            {
-            }
 
             bool SetBossState(uint32 type, EncounterState state)
             {
@@ -480,7 +477,7 @@ class instance_ulduar : public InstanceMapScript
                         if (data == 2)
                         {
                             if (Creature* Leviathan = instance->GetCreature(LeviathanGUID))
-                                Leviathan->AI()->DoAction(10);
+                                Leviathan->AI()->DoAction(ACTION_MOVE_TO_CENTER_POSITION);
                             if (GameObject* gameObject = instance->GetGameObject(LeviathanGateGUID))
                                 gameObject->SetGoState(GO_STATE_ACTIVE_ALTERNATIVE);
                             SaveToDB();
